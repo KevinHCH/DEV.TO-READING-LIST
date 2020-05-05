1460dd696622c10f1543c360982a561a3228d851
Kevin <kevinherbas7@gmail.com>
2020-05-05 19:30:31 +0200

    Complete deploy script + dependencies

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,6 +6,11 @@ PDF_FILES=$(pwd)/pdf/*.pdf
 ## INSTALL DEPENDENCIES ########
 pipenv install
 
+# Install wkhtmltopdf in centos
+# https://gist.github.com/paulsturgess/cfe1a59c7c03f1504c879d45787699f5
+## CREATING ENV_VARS ########
+printf "USERNAME_GIT=\nPASSWORD_GIT=\nMAIL_SENDER=\nMAIL_PASSWORD=\nMAIL_RECEIVER=\n" >> .env
+
 ## CONFIG CRONTAB ########
 crontab -l >> $CRON_NAME
 # Run this command all days at 10:00
